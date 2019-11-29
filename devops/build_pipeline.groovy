602070e44d60f688b14be23f4bb4c058b6abd1b2
praveen-sjsu <praveenthakur.sjsu@gmail.com>
2019-11-28 17:00:32 -0800

    Added pipeline script.

---
+++ devops/build_pipeline.groovy
@@ -0,0 +1,47 @@
+#!/usr/bin/env groovy
+node {
+  def TestServiceJob = 'TestBuyerService'
+  def BuildContainerJob = 'BuildContainer'
+  def DeployServiceJob = 'DeployService'
+  def GitOrganization = "systeam-org"
+  def ServiceGitRepo = "buyer-service"
+  def DockerHubOrganization = "systeamorg"
+  def ServiceDockerHubRepo = "buyer-service"
+  def ServiceBuildNumber = "${env.BUILD_NUMBER}"
+
+
+  stage('Checkout from Git') {
+  		checkout ([
+  		$class: 'GitSCM',
+  		branches: [[name: '*/master']],
+  		extensions: [
+  				[$class: 'CleanCheckout']
+  		],
+  		userRemoteConfigs: [[credentialsId: 'admin', url: "https://github.com/${GitOrganization}/${ServiceGitRepo}"]]
+  		])
+  }
+
+  stage('Execute API Tests') {
+    build_service = build job: "${TestServiceJob}"
+  }
+
+  stage('Build Docker Image') {
+    build_ddr = build job: "${BuildContainerJob}",
+    parameters: [
+        string(name: 'GitOrganization', value: "${GitOrganization}"),
+        string(name: 'ServiceGitRepo', value: "${ServiceGitRepo}"),
+        string(name: 'DockerHubOrganization', value: "${DockerHubOrganization}"),
+        string(name: 'ServiceDockerHubRepo', value: "${ServiceDockerHubRepo}"),
+        string(name: 'ServiceBuildNumber', value: "${ServiceBuildNumber}")
+      ]
+   }
+
+   stage('Deploy Service') {
+    build_ddr = build job: "${DeployServiceJob}",
+    parameters: [
+        string(name: 'GitOrganization', value: "${GitOrganization}"),
+        string(name: 'ServiceGitRepo', value: "${ServiceGitRepo}")
+      ]
+  }
+
+}
---
+++ tests/test_apis.py
@@ -0,0 +1,43 @@
+"""Tests for Basic Functions"""
+import sys
+import json
+import unittest
+import mysql.connector
+sys.path.append("../..")
+from app import *
+
+## Connecting to the database
+
+## importing 'mysql.connector' as mysql for convenient
+import mysql.connector as mysql
+
+class TestFunctions(unittest.TestCase):
+
+    """Test case for the client methods."""
+
+    # Test of get_orders API
+    def test_get_orders(self):
+        with app.test_client() as c:
+            res = c.get('/orders?email=seller@gmail.com')
+
+            # Passing the mock object
+            response = []
+            data = json.loads(res.get_data(as_text=True))
+            # Assert response
+            print(data)
+            self.assertEqual(data, response)
+
+    # Test of get_products API
+    def test_get_products(self):
+        with app.test_client() as c:
+            res = c.get('/products?email=abc@gmail.com')
+
+            # Passing the mock object
+            response = []
+            data = json.loads(res.get_data(as_text=True))
+            # Assert response
+            print(data)
+            self.assertEqual(data, response)
+
+if __name__ == '__main__':
+    unittest.main()
